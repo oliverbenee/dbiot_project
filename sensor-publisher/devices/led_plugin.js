e7bcbb19b7db4ca842bb5fec9b7aa3759399ec92
Oliver <201806928@post.au.dk>
2021-10-30 21:04:35 +0200

    trying to add all sensor stuff in one go.

---
+++ sensor-publisher/devices/led_plugin.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/*
+ * LED plugin. Configured to be plugged into GPIO 4.
+ */ 
+
+const Gpio = require('onoff').Gpio;
+var ledOut;
+
+const led = new Gpio("4", "out");
+
+function toggleLed(){
+    var nv;
+    if(led.readSync() == 0){
+        led.writeSync(1)
+        nv = "1"
+    } else {
+        led.writeSync(0)
+        nv = "0"
+    }
+    publish("", nv) // TODO: publish where?
+}
+
+process.env("SIGINT", () => {
+    led.unexport();
+    process.exit();
+})
+
+module.exports.led = led;
---
+++ sensor-publisher/devices/magsens_plugin.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/*
+ * create magnetic sensor and store in variable occupied
+ */
+var occupied = 0;
+const magSens = new Gpio(22, 'in', 'both')
+magSens.watch((err, value) => {
+    if(err) throw err
+    occupied = 1 - value;
+})
+
+// Listen to the event triggered on CTRL+C, if it get triggered, Cleanly close the GPIO pin before exiting
+process.on('SIGINT', () => {
+    magSens.unexport();
+    console.log('Closing program. Returning to console.')
+    process.exit()
+});
+
+module.exports.occupied = occupied;
