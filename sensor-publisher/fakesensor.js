581e743770e95f05b68acc779c1b8f7e7590a6ef
Oliver <201806928@post.au.dk>
2021-10-31 00:01:55 +0200

    distance_plugin

--- sensor-publisher/fakesensor.js
+++ sensor-publisher/fakesensor.js
@@ -1,5 +1,5 @@
 // Import submodules.
-import { latestDistance } from "./devices/led_plugin.js"
+import { latestDistance } from "./devices/distance_plugin.js"
 //const led = require('./devices/led_plugin')
 //const magsens = require('./devices/magsens_plugin')
 //const distsens = require('./devices/distance_plugin')
