c7dfddb31262deb4f8157b89014d547fd81bef3b
Oliver <201806928@post.au.dk>
2021-10-30 23:35:36 +0200

    try TA pigpio approach

--- sensor-publisher/devices/distance_plugin.js
+++ sensor-publisher/devices/distance_plugin.js
@@ -1,3 +1,53 @@
+import isPi from "detect-rpi";
+import pkg from "pigpio";
+
+const { Gpio, OUTPUT, INPUT } = pkg;
+
+export let latestDistance = null;
+
+try {
+  if (!isPi()) {
+    throw new Error();
+  }
+
+  latestDistance = null;
+  let latestPoll = null;
+  // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
+  const MICROSECDONDS_PER_CM = 1e6 / 34321;
+  const trigger = new Gpio(23, { mode: OUTPUT });
+  const echo = new Gpio(24, { mode: INPUT, alert: true });
+  trigger.digitalWrite(0); // Make sure trigger is low
+  function watchHCSR04() {
+    let startTick;
+    echo.on("alert", (level, tick) => {
+      if (level === 1) {
+        startTick = tick;
+      } else {
+        const endTick = tick;
+        const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+        const distance = diff / 2 / MICROSECDONDS_PER_CM;
+        latestDistance = distance;
+        latestPoll = new Date().getTime();
+      }
+    });
+  }
+  watchHCSR04();
+  // Trigger a distance measurement once per second
+  setInterval(
+    () => trigger.trigger(10, 1), // Set trigger high for 10 microseconds
+    250
+  );
+  // check for errors
+  setInterval(() => {
+    if (new Date().getTime() - latestPoll > 1000) {
+      latestDistance = null;
+    }
+  }, 1000);
+} catch (error) {
+  console.log("Error in ultrasonic");
+  latestDistance = null;
+}
+
 'use strict'
 ///// const PigPio = require('pigpio').Gpio;
 
@@ -10,7 +60,7 @@
 ///// const echo = new PigPio(24, {mode: PigPio.INPUT, alert: true});
 ///// trigger.digitalWrite(0); // Make sure trigger is low
 
-var dist = 0; // store distance value. 
+//var dist = 0; // store distance value.  TODO: UNCOMMENT THIS LINE FOR DEBUGGING.
 ///// function watchHCSR04(){
 /////     let startTick;
 /////     echo.on('alert', (level, tick) => {
@@ -29,12 +79,12 @@ var dist = 0; // store distance value.
 /////     trigger.trigger(10, 1); // Set trigger high for 10 microseconds
 ///// }, 3000);
 
-setInterval(() => {
-  dist += 1.1
-}, 1000)
+//setInterval(() => { TODO: UNCOMMENT THIS FUNCTION FOR DEBUGGING.
+//  dist += 1.1
+//}, 1000)
 
 exports.getDistance = () => {
-  return dist;
+  return latestDistance;
 }
   
 exports.printproof = function() {
