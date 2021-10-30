a85c575e681d64dfd57684d397b149a0116a16b9
Oliver <201806928@post.au.dk>
2021-10-30 21:52:15 +0200

    more print statements for testing

--- sensor-publisher/devices/magsens_plugin.js
+++ sensor-publisher/devices/magsens_plugin.js
@@ -14,7 +14,9 @@ const magSens = new Gpio(22, 'in', 'both')
 magSens.watch((err, value) => {
     if(err) throw err
     console.log("VALUE CHANGE IN MAGSENS")
+    console.log("old occupied value: " + occupied)
     occupied = 1 - value;
+    console.log("new occupied value: " + occupied)
 })
 
 // Listen to the event triggered on CTRL+C, if it get triggered, Cleanly close the GPIO pin before exiting
