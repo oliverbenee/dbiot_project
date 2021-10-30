440335ecb3b53417d88f1a3461f8cab8f0dca624
Oliver <201806928@post.au.dk>
2021-10-31 01:39:31 +0200

    comment out everything distance sensor related for testing.

--- sensor-publisher/devices/distance_plugin.js
+++ sensor-publisher/devices/distance_plugin.js
@@ -1,6 +1,6 @@
-import pkg from "pigpio";
+//import pkg from "pigpio";
 
-const { Gpio, OUTPUT, INPUT } = pkg;
+//const { Gpio, OUTPUT, INPUT } = pkg;
 
 export let latestDistance = null;
 
@@ -9,29 +9,29 @@ try {
   let latestPoll = null;
   // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
   const MICROSECDONDS_PER_CM = 1e6 / 34321;
-  const trigger = new Gpio(23, { mode: OUTPUT });
-  const echo = new Gpio(24, { mode: INPUT, alert: true });
-  trigger.digitalWrite(0); // Make sure trigger is low
-  function watchHCSR04() {
-    let startTick;
-    echo.on("alert", (level, tick) => {
-      if (level === 1) {
-        startTick = tick;
-      } else {
-        const endTick = tick;
-        const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-        const distance = diff / 2 / MICROSECDONDS_PER_CM;
-        latestDistance = distance;
-        latestPoll = new Date().getTime();
-      }
-    });
-  }
-  watchHCSR04();
+//  const trigger = new Gpio(23, { mode: OUTPUT });
+//  const echo = new Gpio(24, { mode: INPUT, alert: true });
+//  trigger.digitalWrite(0); // Make sure trigger is low
+//  function watchHCSR04() {
+//    let startTick;
+//    echo.on("alert", (level, tick) => {
+//      if (level === 1) {
+//        startTick = tick;
+//      } else {
+//        const endTick = tick;
+//        const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+//        const distance = diff / 2 / MICROSECDONDS_PER_CM;
+//        latestDistance = distance;
+//        latestPoll = new Date().getTime();
+//      }
+//    });
+//  }
+//  watchHCSR04();
   // Trigger a distance measurement once per second
-  setInterval(
-    () => trigger.trigger(10, 1), // Set trigger high for 10 microseconds
-    250
-  );
+//  setInterval(
+//    () => trigger.trigger(10, 1), // Set trigger high for 10 microseconds
+//    250
+//  );
   // check for errors
   setInterval(() => {
     if (new Date().getTime() - latestPoll > 1000) {
@@ -74,9 +74,9 @@ try {
 /////     trigger.trigger(10, 1); // Set trigger high for 10 microseconds
 ///// }, 3000);
 
-//setInterval(() => { TODO: UNCOMMENT THIS FUNCTION FOR DEBUGGING.
-//  dist += 1.1
-//}, 1000)
+setInterval(() => { //TODO: UNCOMMENT THIS FUNCTION FOR DEBUGGING.
+  dist += 1.1
+}, 1000)
 
 exports.printproof = function() {
   console.log("Sensor merger and distance sensor plugin has connection.")
