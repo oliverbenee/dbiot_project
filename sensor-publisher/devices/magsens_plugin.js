fac8df8804513e47c77cb2f38b8c6d711720e081
Oliver <201806928@post.au.dk>
2021-10-30 22:11:01 +0200

    try to remove debug script

--- sensor-publisher/devices/magsens_plugin.js
+++ sensor-publisher/devices/magsens_plugin.js
@@ -8,8 +8,6 @@ console.log("magsens plugin is running!");
  */
 var occupied = 0;
 
-setInterval(() => {occupied += 1}, 3000)
-
 const magSens = new Gpio(22, 'in', 'both')
 magSens.watch((err, value) => {
     if(err) throw err
