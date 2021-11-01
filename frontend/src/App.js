c71517dd3902e2114ba124a305ea4dfbb3aed453
Florian Phillip Flöter <2433-au698443@users.noreply.gitlab.au.dk>
2021-11-01 18:55:39 +0100

    Added linechart for capacity overview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import History from "./components/pages/history.component";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import GarageDetails from "./components/pages/garage.detail.component";
 
+const array1 = [25, 50, 35, 15, 94, 10, 40];
+const array2 = [45, 60, 15, 45, 74, 5, 30];
+const array3 = [34, 64, 20, 10, 10, 35, 60];
+
 function App() {
   return (
     <BrowserRouter>
--- frontend/src/components/pages/homepage.component.js
+++ frontend/src/components/pages/homepage.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Garage from "./garage.component";
 import "./homepage.css";
+import * as d3 from "d3";
 
 /**
  * Main Component to display an overview of all parking garages in aarhus
@@ -12,8 +13,12 @@ const API_URL_OPENDATA_PARKING_GARAGES =
 export default class Homepage extends Component {
   constructor(props) {
     super(props);
+    this.myRef = React.createRef();
     this.state = {
       garages: [],
+      array1: [25, 50, 35, 15, 94, 10, 40],
+      array2: [45, 60, 15, 45, 74, 5, 30],
+      array3: [34, 64, 20, 10, 10, 35, 60],
     };
   }
 
@@ -22,6 +27,77 @@ export default class Homepage extends Component {
       .then((response) => response.json())
       .then((data) => this.setState({ garages: data.result.records }))
       .catch(console.error());
+
+    this.drawLineChart();
+  }
+
+  drawLineChart() {
+    // setting up svg
+    const w = 600;
+    const h = 300;
+    const svg = d3
+      .select(this.myRef.current)
+      .attr("width", w)
+      .attr("height", h)
+      .style("background", "#d3d3d3")
+      .style("margin-left", "100")
+      .style("margin-bottom", "30")
+      .style("overflow", "visible");
+
+    // setting the scaling
+
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, this.state.array1.length - 1])
+      .range([0, w]);
+
+    const yScale = d3.scaleLinear().domain([0, h]).range([h, 0]);
+
+    const generatedScaledLine = d3
+      .line()
+      .x((d, i) => xScale(i))
+      .y(yScale)
+      .curve(d3.curveCardinal);
+
+    const generatedScaledLine1 = d3
+      .line()
+      .x((d, i) => xScale(i))
+      .y(yScale)
+      .curve(d3.curveCardinal);
+    // setting axes
+    const xAxis = d3
+      .axisBottom(xScale)
+      .ticks(this.state.array1.length)
+      .tickFormat((i) => i + 1);
+
+    const yAxis = d3.axisLeft(yScale).ticks(5);
+
+    svg.append("g").call(xAxis).attr("transform", `translate(0, ${h})`);
+    svg.append("g").call(yAxis);
+    //setting up the data
+    svg
+      .selectAll(".line")
+      .data([this.state.array1])
+      .join("path")
+      .attr("d", (d) => generatedScaledLine(d))
+      .attr("fill", "none")
+      .attr("stroke", "blue");
+
+    svg
+      .selectAll(".line")
+      .data([this.state.array2])
+      .join("path")
+      .attr("d", (d) => generatedScaledLine1(d))
+      .attr("fill", "none")
+      .attr("stroke", "red");
+
+    svg
+      .selectAll(".line")
+      .data([this.state.array3])
+      .join("path")
+      .attr("d", (d) => generatedScaledLine1(d))
+      .attr("fill", "none")
+      .attr("stroke", "orange");
   }
 
   garageList() {
@@ -36,53 +112,23 @@ export default class Homepage extends Component {
     });
   }
 
-  // toggleLed(e) {
-  //   // send request to toggle LED
-  //   switch (e) {
-  //     case 1:
-  //       if (this.state.led_1 == 1) {
-  //         this.setState({ led_1: 0 });
-  //       } else {
-  //         this.setState({ led_1: 1 });
-  //       }
-  //       break;
-  //     case 2:
-  //       if (this.state.led_2 == 1) {
-  //         this.setState({ led_2: 0 });
-  //       } else {
-  //         this.setState({ led_2: 1 });
-  //       }
-  //       break;
-  //     case 3:
-  //       if (this.state.led_3 == 1) {
-  //         this.setState({ led_3: 0 });
-  //       } else {
-  //         this.setState({ led_3: 1 });
-  //       }
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
-
   /** render component */
   render() {
     return (
       <div class="container">
-        <div>
-          <header class="jumbotron my-4">
-            <h3 class="display-3">Parking Aarhus</h3>
-          </header>
-          <body>
-            <table>
-              <thead>
-                <th>Garage </th>
-                <th>Capacity </th>
-              </thead>
-              {this.garageList()}
-            </table>
-          </body>
-        </div>
+        <header class="jumbotron my-4">
+          <h3 class="display-3">Parking Aarhus</h3>
+        </header>
+        <body>
+          <table>
+            <thead>
+              <th>Garage </th>
+              <th>Capacity </th>
+            </thead>
+            {this.garageList()}
+          </table>
+        </body>
+        <svg ref={this.myRef}></svg>
       </div>
     );
   }
