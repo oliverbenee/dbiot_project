0e42ae5727ffe8a20e5acdb0700ab3cdfa4ba7a1
Oliver <201806928@post.au.dk>
2021-11-23 20:10:11 +0100

    comment out.

--- backend/mysql/db.js
+++ backend/mysql/db.js
@@ -8,41 +8,44 @@ const pool = mysql.createPool({
   database: "buildingiot",
 });
 
+// notes: 
+// - without this code. error: Table buildingiot.parkingSlot does not exist.
+// - with this code for creating the table: TABLE_EXISTS_ERROR.
 // FIXME: delete. Should use dump.sql. Breaks again on my machine and i don't know how to fix it. 
-pool.getConnection((err, connection) => {
-  if (err) throw err
-  connection.query(
-    `CREATE TABLE parkingZone
-    ( parkingZoneID VARCHAR(50),
-      PRIMARY KEY (parkingZoneID),
-      latitude FLOAT(4,2),
-      longitude FLOAT(4,2),
-      totalCapacity INT(4),
-      freeSlots INT(4)
-)`, (err) => {
-      if (err) throw err
-    })
-  connection.query(
-    `CREATE TABLE parkingSlot
-    ( slotID INT(10),
-      isOccupied BOOLEAN,
-      parkingZoneID VARCHAR(50),
-      FOREIGN KEY (parkingZoneID) REFERENCES parkingZone(parkingZoneID),
-      PRIMARY KEY(slotID, parkingZoneID)
-)`, (err) => {
-      if (err) throw err
-    })
-  connection.query(
-    `CREATE TABLE historical
-    (time timestamp default current_timestamp,
-    parkingZoneID VARCHAR(50),
-    freeSlots INT(4),
-    totalCapacity INT(4)
-  )`, (err) => {
-      if (err) throw err
-    })
-  connection.release()
-})
+// pool.getConnection((err, connection) => {
+//   if (err) throw err
+//   connection.query(
+//     `CREATE TABLE IF NOT EXISTS parkingZone
+//     ( parkingZoneID VARCHAR(50),
+//       PRIMARY KEY (parkingZoneID),
+//       latitude FLOAT(4,2),
+//       longitude FLOAT(4,2),
+//       totalCapacity INT(4),
+//       freeSlots INT(4)
+// )`, (err) => {
+//       if (err) throw err
+//     })
+//   connection.query(
+//     `CREATE TABLE IF NOT EXISTS parkingSlot
+//     ( slotID INT(10),
+//       isOccupied BOOLEAN,
+//       parkingZoneID VARCHAR(50),
+//       FOREIGN KEY (parkingZoneID) REFERENCES parkingZone(parkingZoneID),
+//       PRIMARY KEY(slotID, parkingZoneID)
+// )`, (err) => {
+//       if (err) throw err
+//     })
+//   connection.query(
+//     `CREATE TABLE IF NOT EXISTS historical
+//     (time timestamp default current_timestamp,
+//     parkingZoneID VARCHAR(50),
+//     freeSlots INT(4),
+//     totalCapacity INT(4)
+//   )`, (err) => {
+//       if (err) throw err
+//     })
+//   connection.release()
+// })
 
 class Database {
   // get data
