da15395e9cb2be308c745639150acdda23b95076
Florian Phillip Flöter <2433-au698443@users.noreply.gitlab.au.dk>
2021-12-01 13:52:25 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ client.on("message", function (topic, message, packet) {
     navigation.setState(spotNumber, isOccupied);
   }
   if (topic == "home/sensor/tripwire") {
-    setTimeout(() => publishAvailableParkingSlot(), 5000);
+    publishAvailableParkingSlot()
   }
   // console.log("___________________________"); //UNCOMMENT THIS LINE FOR DEBUG
   // DEBUG: goto localhost:5000/parkingslots/KALKVAERKSVEJ for checking
